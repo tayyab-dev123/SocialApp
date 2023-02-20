e367dfd7de4de45ecacfc17cb65e669ff2452c22
Tayyab <tayyab.shahzad100000@gmail.com>
2023-02-21 03:51:05 +0500

    Api Created For SocialApp Project

---
+++ Data/DataContext.cs
@@ -0,0 +1,16 @@
+using Microsoft.EntityFrameworkCore;
+using SocialApp.Entities;
+
+namespace SocialApp.Data
+{
+    public class DataContext : DbContext
+    {
+         public DataContext(DbContextOptions options) : base(options)
+         {
+            
+         }
+
+         public DbSet<AppUser> Users {get; set;}
+         
+    }
+}
