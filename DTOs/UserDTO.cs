9641d8703cdf35a6d0e93ab430cb537f2783cef5
Tayyab <tayyab.shahzad100000@gmail.com>
2023-02-27 16:35:18 +0500

    BuggyController to test errors added

--- DTOs/UserDTO.cs
+++ DTOs/UserDTO.cs
@@ -1,8 +1,11 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace SocialApp.DTOs
 {
     public class UserDTO
     {
         public string UserName { get; set; }
+
         public string Token { get; set; }
     }
 }
